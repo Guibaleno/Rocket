c4dbd9feefbda0954572ff2fc30d43e419b7ae9b
B533P-21\533 <glambertgm@gmail.com>
2018-08-24 16:53:35 -0400

    Choix du mauvais fichier, changement de fichier

---
+++ Assets/ScriptRocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRocket : MonoBehaviour {
+    public Rigidbody CarRigidBody;
+    [SerializeField] public float ForceUpward = 200;
+    [SerializeField] public float ForceTrust = 200;
+    // Use this for initialization
+    void Start () {
+        CarRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ManageVerticalInput();
+        ManageHorizontalInput();
+    }
+    private void ManageVerticalInput()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            CarRigidBody.AddRelativeForce(new Vector3(0, ForceUpward, 0));
+        }
+        else if (Input.GetAxis("Jump") < 0)
+        {
+            CarRigidBody.AddRelativeForce(new Vector3(0, -ForceUpward, 0));
+        }
+    }
+    private void ManageHorizontalInput()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            CarRigidBody.AddForceAtPosition(new Vector3(0, 0, ForceTrust), new Vector3());
+            
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            CarRigidBody.AddForceAtPosition(new Vector3(0, 0, -ForceTrust), new Vector3());
+        }
+    }
+}
